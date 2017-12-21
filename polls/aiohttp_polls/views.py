9ab4a4e02c608c0752fb8e731f83419b39859266
Dmitry Sichkar <dmmeteo@gmail.com>
2017-12-22 00:31:15 +0200

    bu

--- polls/aiohttp_polls/views.py
+++ polls/aiohttp_polls/views.py
@@ -2,7 +2,7 @@ from aiohttp import web
 
 
 async def index(request):
-    return web.Response(text='Hello, Aiohttp!')
+    return web.Response(text='Hello, Boba!')
 
 async def hello(request):
     return web.Response(text='Hello, Trololo!')
